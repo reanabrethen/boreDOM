64d51aeea103e18c92357c12421c60add5c224cb
Admin <reanajb@gmail.com>
2024-01-16 22:44:03 -0500

    completed

---
+++ /main.js
@@ -0,0 +1,45 @@
+
+
+
+let fontColor = document.querySelector('.font-color')
+fontColor.style.color = 'aqua'
+
+let heading = document.querySelector('#heading')
+heading.style.fontSize = '42px'
+
+let newText = document.querySelector('#new-text')
+newText.innerHTML = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id urna a libero interdum posuere. Donec vitae aliquet sapien. Vivamus sagittis erat a lacus tincidunt, vel feugiat odio ultrices.</p>'
+
+let item12 = document.querySelector('#item-12')
+item12.style.color = 'rgba(255, 99, 71, .3)'
+
+let item2 = document.querySelector('#item-2')
+item2.innerHTML = ' <li class="item" id="item-3">Greetings!</li>'
+
+let image = document.querySelector('img')
+image.src = 'https://media.istockphoto.com/id/1470130937/photo/young-plants-growing-in-a-crack-on-a-concrete-footpath-conquering-adversity-concept.webp?b=1&s=170667a&w=0&k=20&c=IRaA17rmaWOJkmjU_KD29jZo4E6ZtG0niRpIXQN17fc='
+image.style.height = '250px'
+
+let img2 = document.querySelector('.imgDiv')
+img2.innerHTML += '<img src="https://s1.cdn.autoevolution.com/images/news/gallery/1998-subaru-impreza-wrc-with-colin-mcrae-ties-and-interesting-life-is-for-sale_1.jpg"/>'
+img2.innerHTML.style.height = '280px'
+
+
+let list = document.querySelector('ul')
+list.innerHTML = '<li class="item" id="item-1">1</li>'
+list.innerHTML += '<li class="item" id="item-2">2</li>'
+list.innerHTML += '<li class="item" id="item-3">3</li>'
+list.innerHTML += '<li class="item" id="item-4">4</li>'
+list.innerHTML += '<li class="item" id="item-5">5</li>'
+list.innerHTML += '<li class="item" id="item-6">6</li>'
+list.innerHTML += '<li class="item" id="item-7">7</li>'
+list.innerHTML += '<li class="item" id="item-8">8</li>'
+list.innerHTML += '<li class="item" id="item-9">9</li>'
+list.innerHTML += '<li class="item" id="item-10">10</li>'
+list.innerHTML += '<li class="item" id="item-11">11</li>'
+list.innerHTML += '<li class="item" id="item-12">12</li>'
+list.innerHTML += '<li class="item" id="item-13">13</li>'
+list.innerHTML += '<li class="item" id="item-14">14</li>'
+list.innerHTML += '<li class="item" id="item-15">15</li>'
+list.innerHTML += '<li class="item" id="item-16">16</li>'
+list.innerHTML += '<li class="item" id="item-17">"Never gonna give you up.\"</li>'
